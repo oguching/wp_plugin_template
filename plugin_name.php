d6d80844251c862cf424698c93d5bbf0618163ac
oguchi <n.zekwe@gmail.com>
2016-11-23 16:22:44 +0100

    Create plugin_name.php
    added plugin header

---
+++ plugin_name.php
@@ -0,0 +1,11 @@
+<?php
+  /*
+  Plugin Name: Your Plugin Name
+  Description: Describe Your Plugin Example: Hell
+  Version: 1.0
+  Author: Author's name
+  Author URI: http://yoursite.com/
+  Plugin URI: http://yoursite.com/plugin-name
+  */
+
+?>
